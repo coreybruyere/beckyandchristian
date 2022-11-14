de9d8015c18b8c29c9e3b6200fab88e336046706
Corey Bruyere <corey@sundae.com>
2022-11-14 12:21:16 -0800

    initial setup

---
+++ astro.config.mjs
@@ -0,0 +1,11 @@
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import astroI18next from "astro-i18next";
+
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://example.com",
+  integrations: [mdx(), sitemap(), astroI18next()],
+});
