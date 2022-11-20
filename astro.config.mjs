0667265173480c3b28abeb81fdb26a6ea3be16e7
Corey Bruyere <coreybruyere@ymail.com>
2022-11-19 20:16:59 -0800

    GA

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
+  site: "https://beckyandchristian.com/",
   integrations: [mdx(), sitemap(), astroI18next()],
 });
