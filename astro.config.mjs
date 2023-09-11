bc74fafcf46acc591fbb55798fcb6a0ea69d4c94
Corey Bruyere <coreybruyere@ymail.com>
2023-09-10 17:28:21 -0700

    feat: Add entire website

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://beckyandchristian.com/",
+  site: "https://christianandalyssa.com/",
   integrations: [mdx(), sitemap(), astroI18next()],
 });
